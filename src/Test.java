52c6e7658ae5c42cdfa84b3a161368331edeba35
hanjiwen <flyjwhan@163.com>
2021-02-20 16:55:29 +0800

    0220hanjw

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++) {
+            System.out.println("This is my " + i + " program,Hello World!");
+        }
+    }
+
+}
